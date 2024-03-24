a2cdd3f57a1882454e173ca20fc86513b176854d
Yi-Qing <yiqing.wang.sc@foxmail.com>
2024-03-24 17:45:52 +0800

    cmake project demo

--- demo/project/cmake/main/src/main.c
+++ demo/project/cmake/main/src/main.c
@@ -5,6 +5,8 @@
 
 int main(int argc, char **argv)
 {
+    (void)argc;
+    (void)argv;
     char buf[40] = { 0 };
     printf("Ver: %s\n", cdf_get_version(buf, 40));
     cdf_hello_hello();
--- demo/project/cmake/modules/module_world/src/world.c
+++ demo/project/cmake/modules/module_world/src/world.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include "say.h"
 #include "world.h"
+#include "say.h"
+#include <stdio.h>
 
 void cdf_world_world()
 {
     printf("ret: %d\n", cdf_world_say("world"));
+    printf("ret: %d\n", cdf_world_say("世界"));
 }
-
